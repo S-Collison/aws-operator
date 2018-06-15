e224246eab8ad84129d060b07a21d02ecad269eb
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-06-15 12:09:20 +0300

    Add v14

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -8,6 +8,7 @@ import (
 	"github.com/giantswarm/aws-operator/service/controller/v11"
 	"github.com/giantswarm/aws-operator/service/controller/v12"
 	"github.com/giantswarm/aws-operator/service/controller/v13"
+	"github.com/giantswarm/aws-operator/service/controller/v14"
 	"github.com/giantswarm/aws-operator/service/controller/v2"
 	"github.com/giantswarm/aws-operator/service/controller/v3"
 	"github.com/giantswarm/aws-operator/service/controller/v4"
@@ -40,6 +41,7 @@ func NewVersionBundles() []versionbundle.Bundle {
 	versionBundles = append(versionBundles, v11.VersionBundle())
 	versionBundles = append(versionBundles, v12.VersionBundle())
 	versionBundles = append(versionBundles, v13.VersionBundle())
+	versionBundles = append(versionBundles, v14.VersionBundle())
 
 	return versionBundles
 }
