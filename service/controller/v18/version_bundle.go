b134d185967f0ffff92c37e54440570001ebed1d
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-10-02 14:45:10 +0300

    Bump aws-operator to new major version

--- service/controller/v18/version_bundle.go
+++ service/controller/v18/version_bundle.go
@@ -8,34 +8,9 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "aws-operator",
-				Description: "Add security group descriptions for master nodes.",
+				Component:   "clouconfig",
+				Description: "Switch cloudconfig from cloudinit to ignition.",
 				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "cloudconfig",
-				Description: "Removed CoreDNS related components (now managed by chart-operator).",
-				Kind:        versionbundle.KindRemoved,
-			},
-			{
-				Component:   "aws-operator",
-				Description: "Set higher timeouts for NVME driver.",
-				Kind:        versionbundle.KindAdded,
-			},
-			{
-				Component:   "calico",
-				Description: "Updated calico to 3.2.0.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Patched Kubernetes v1.11.1 with apimachinery fix to plug apiserver memory leak.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "aws-operator",
-				Description: "Set higher timeouts for NVME driver.",
-				Kind:        versionbundle.KindAdded,
 			},
 		},
 		Components: []versionbundle.Component{
@@ -65,6 +40,6 @@ func VersionBundle() versionbundle.Bundle {
 			},
 		},
 		Name:    "aws-operator",
-		Version: "4.2.0",
+		Version: "5.0.0",
 	}
 }
