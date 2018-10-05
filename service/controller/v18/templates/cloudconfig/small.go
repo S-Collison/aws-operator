ef24ca08be8e8f8ce7e7c79a49c088d56c11c587
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-10-05 13:38:10 +0300

    Add filesystems for aws

--- service/controller/v18/templates/cloudconfig/small.go
+++ service/controller/v18/templates/cloudconfig/small.go
@@ -4,10 +4,30 @@ const Small = `{
   "ignition": {
     "version": "2.2.0",
     "config": {
-      "replace": {
+      "append": {
         "source": "{{ .S3URL }}"
       }
     }
+  },
+  "storage": {
+    "filesystems": [
+      {
+        "mount": {
+          "device": "/dev/nvme1n1",
+          "format": "xfs",
+          "label": "docker"
+        },
+        "name": "docker"
+      },
+      {
+        "mount": {
+          "device": "/dev/nvme2n1",
+          "format": "ext4",
+          "label": "etcd"
+        },
+        "name": "var-lib-etcd"
+      }
+    ]
   }
 }
 `
